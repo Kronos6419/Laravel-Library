a528403e27f795cc4dcdae887ea52cab513f87d8
yousu <yousufsubzwari6419@gmail.com>
2026-05-26 23:55:08 +1200

    home and register routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/','posts.index')->name('home');
+
+Route::view('/register','auth.register')->name('register');
+
+// Route::get('/', function () {
+//     return view('posts.index');
+// })->name('home');
+
+// Route::get('/register', function () {
+//     return view('auth.register');
+// })->name('register');
