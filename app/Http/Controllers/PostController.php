60d68ca72b3210aee4125140a6434986ad000d43
yousu <yousufsubzwari6419@gmail.com>
2026-06-10 10:16:49 +1200

    Add post ownership authorization

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,10 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
-class PostController extends Controller
+class PostController extends Controller implements HasMiddleware
 {
+    public static function middleware(): array
+    {
+        return [
+            new Middleware('auth', except: ['index', 'show']),
+        ];
+    }
+
     /**
      * Display a listing of the resource.
      */
@@ -55,6 +65,9 @@ class PostController extends Controller
      */
     public function edit(Post $post)
     {
+        // Authorizing the action
+        Gate::authorize('modify', $post);
+
         return view('posts.edit', ['post' => $post]);
     }
 
@@ -63,6 +76,9 @@ class PostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
+        // Authorizing the action
+        Gate::authorize('modify', $post);
+
         $fields = $request->validate([
             'title' => ['required', 'max:255'],
             'body' => ['required'],
@@ -79,6 +95,9 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
+        // Authorizing the action
+        Gate::authorize('modify', $post);
+
         $post->delete();
         return back()->with('delete', 'Your post was deleted');
     }
